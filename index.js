3480cfafdaa89dd1b914034f762b25c4b688323e
endyranaudo <endyranaudo@gmail.com>
2019-05-14 07:11:49 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+// WRITING CARDS
+
+function writeCards(names_arr, event) {
+  let cards = []
+  for (let i = 0; i < names_arr.length; i++ ) {
+    cards.push(`Thank you, ${names_arr[i]}, for the wonderful ${event} gift!`)
+  }
+  return cards
+}
+
+
+
+// COUNTDOWN
+function countdown(countdown){
+  while (countdown > 0) {
+    console.log(countdown);
+    countdown -= 1;
+  }
+  console.log(countdown);
+}
